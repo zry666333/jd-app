fab88d2307fa41a74d6cde0055e65f60f4060598
zry <13262512337@163.com>
2019-08-24 11:56:09 +0800

    项目上传

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -3,6 +3,8 @@
         <Heador/>
         <Slider/>
         <Novice/>
+        <Borrow/>
+        <Money/>
     </div>
 </template>
 
@@ -10,11 +12,15 @@
     import Heador from "../public/header.vue"
     import Slider from "./hslider.vue"
     import Novice from "./novice.vue"
+    import Borrow from "./borrow.vue"
+    import Money from "./money.vue"
     export default {
         components:{
             Heador,
             Slider,
             Novice,
+            Borrow,
+            Money,
         }
     }
 </script>
